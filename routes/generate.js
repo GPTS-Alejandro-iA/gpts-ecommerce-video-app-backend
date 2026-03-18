6658f99f179caa0ee619c392e49f410449d5dc5d
Green Power Tech Store LLC <gpts.citas@gmail.com>
2026-03-17 20:33:24 -0400

    Add video generation route with file upload

--- routes/generate.js
+++ routes/generate.js
@@ -1,1 +1,10 @@
+import express from "express";
+import upload from "../middleware/upload.js";
+import { generateVideoController } from "../controllers/generateController.js";
+
+const router = express.Router();
+
+router.post("/", upload.single("file"), generateVideoController);
+
+export default router;
 
