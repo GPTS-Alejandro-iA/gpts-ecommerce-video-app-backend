bbb0f2b8bcf5e2d4cedaf927db19ce4f6cb66043
Green Power Tech Store LLC <gpts.citas@gmail.com>
2026-03-19 02:57:32 -0400

    Implement image and video generation functions
    Added functions to generate images and videos using the Runpod API.

--- controllers/runpodController.js
+++ controllers/runpodController.js
@@ -1,1 +1,46 @@
+import fetch from "node-fetch";
+
+const RUNPOD_ENDPOINT_URL = process.env.RUNPOD_ENDPOINT_URL;
+const RUNPOD_API_KEY = process.env.RUNPOD_API_KEY;
+
+// 1. Generar imagen
+export async function generarImagen(prompt) {
+  const response = await fetch(RUNPOD_ENDPOINT_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${RUNPOD_API_KEY}`
+    },
+    body: JSON.stringify({
+      input: {
+        type: "image",
+        prompt
+      }
+    })
+  });
+
+  const data = await response.json();
+  return data;
+}
+
+// 2. Generar video desde imagen
+export async function generarVideoDesdeImagen(imageUrl, prompt) {
+  const response = await fetch(RUNPOD_ENDPOINT_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${RUNPOD_API_KEY}`
+    },
+    body: JSON.stringify({
+      input: {
+        type: "video",
+        image: imageUrl,
+        prompt
+      }
+    })
+  });
+
+  const data = await response.json();
+  return data;
+}
 
