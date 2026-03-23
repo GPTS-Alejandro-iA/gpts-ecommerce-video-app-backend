475723d0bf0a01291d938930fa6a0b534237a665
Green Power Tech Store LLC <gpts.citas@gmail.com>
2026-03-22 23:25:13 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,17 @@
-module.exports = async (req, res) => {
-  return res.json({
-    status: "ok",
-    message: "Serverless handler funcionando."
-  });
+import runpod from "runpod";
+
+export const handler = async (event) => {
+  console.log("Evento recibido:", event);
+
+  return {
+    status: "success",
+    message: "Handler de RunPod funcionando correctamente.",
+    input: event
+  };
 };
+
+runpod.serverless.start({
+  handler
+});
+
 
