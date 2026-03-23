b7bee46c3838b78c3dcb574b8beac5cef9028ce2
Green Power Tech Store LLC <gpts.citas@gmail.com>
2026-03-22 23:34:11 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,11 +1,9 @@
 import runpod from "runpod";
 
 export const handler = async (event) => {
-  console.log("Evento recibido:", event);
-
   return {
     status: "success",
-    message: "Handler de RunPod funcionando correctamente.",
+    message: "Handler funcionando.",
     input: event
   };
 };
